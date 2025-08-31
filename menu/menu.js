caaba04578532b432a741ec74cdb4e04bc30029e
Guilherme <joaoguilhermebomfim60@gmail.com>
2025-08-31 11:33:10 -0300

    update

--- menu/menu.js
+++ menu/menu.js
@@ -1,1 +1,11 @@
 
+aparecer = () => {
+  let menu = document.getElementById('menu');
+  let icon = document.getElementById('icon');
+
+  if(menu.style.display === 'none'){
+    menu.style.display = 'flex';
+  }else{
+    menu.style.display = 'none';
+  }
+}
